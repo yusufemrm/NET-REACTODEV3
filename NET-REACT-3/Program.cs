d8773ec757d5c213e5dfdc807cd24c5ee83079a9
yusuf emrem <emrem.yusuf1@gmail.com>
2025-03-06 23:43:12 +0300

    ödevin devamı

---
+++ NET-REACT-3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+// Abstract sınıf örneği
+public abstract class Animal
+{
+    public string Name { get; set; }
+
+    public Animal(string name) 
+    {
+        Name = name;
+    }
+
+
+    public abstract void MakeSound();
+
+
+    public void Sleep()
+    {
+        Console.WriteLine($"{Name} is sleeping...");
+    }
+}
+
+
+public class Dog : Animal
+{
+    public Dog(string name) : base(name) 
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} says: Woof Woof!");
+    }
+}
+
+
+public class Cat : Animal
+{
+    public Cat(string name) : base(name) 
+    {
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} says: Meow Meow!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal dog = new Dog("Buddy");
+        dog.MakeSound();  
+        dog.Sleep();      
+
+        Animal cat = new Cat("Kitty");
+        cat.MakeSound();  
+        cat.Sleep();      
+    }
+}
---
+++ NET-REACT3-4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+// Interface örneği
+public interface IVehicle
+{
+    void Start();
+    void Stop();
+}
+
+
+public class Car : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Car is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car is stopping...");
+    }
+}
+
+
+public class Bicycle : IVehicle
+{
+    public void Start()
+    {
+        Console.WriteLine("Bicycle is starting...");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Bicycle is stopping...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        IVehicle car = new Car();
+        car.Start();  
+        car.Stop();   
+
+        Console.WriteLine();
+
+        IVehicle bicycle = new Bicycle();
+        bicycle.Start();  
+        bicycle.Stop();   
+    }
+}
